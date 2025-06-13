f26aef7db5283d27d13155ea5d44d0123a17d309
Arun Nautiyal <nautiyalar@gmail.com>
2025-06-13 23:55:48 +0530

    Initial backend setup for Render

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
-import pyodbc
+# import pyodbc
 
+# class Config:
+#     SQL_SERVER = '103.25.174.53'  # e.g., 'localhost' or 'server_ip'
+#     DATABASE = 'pragatiweb'
+#     USERNAME = 'sa'
+#     PASSWORD = 'India@123456#'
+#     DRIVER = '{ODBC Driver 17 for SQL Server}'
+#     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+import os
 class Config:
-    SQL_SERVER = '103.25.174.53'  # e.g., 'localhost' or 'server_ip'
-    DATABASE = 'pragatiweb'
-    USERNAME = 'sa'
-    PASSWORD = 'India@123456#'
+    SQL_SERVER = os.getenv('SQL_SERVER', '103.25.174.53')
+    DATABASE = os.getenv('DATABASE', 'pragatiweb')
+    USERNAME = os.getenv('DB_USERNAME')
+    PASSWORD = os.getenv('DB_PASSWORD')
     DRIVER = '{ODBC Driver 17 for SQL Server}'
     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
