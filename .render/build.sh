bee80ef8e7b0bdf22054598a07133c3e23fc5569
Arun Nautiyal <nautiyalar@gmail.com>
2025-06-14 13:44:33 +0530

    Update build.sh with FreeTDS debug

--- .render/build.sh
+++ .render/build.sh
@@ -1,10 +1,16 @@
-echo #!/bin/bash > .render\build.sh
-echo apt-get update >> .render\build.sh
-echo apt-get install -y unixodbc unixodbc-dev freetds-bin freetds-dev tdsodbc >> .render\build.sh
-echo echo \"[FreeTDS]\" \> /etc/odbcinst.ini >> .render\build.sh
-echo echo \"Description=FreeTDS ODBC Driver\" \>> /etc/odbcinst.ini >> .render\build.sh
-echo echo \"Driver=/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so\" \>> /etc/odbcinst.ini >> .render\build.sh
-echo echo \"Setup=/usr/lib/x86_64-linux-gnu/odbc/libtdsS.so\" \>> /etc/odbcinst.ini >> .render\build.sh
-echo echo \"TDS_Version=7.4\" \>> /etc/odbcinst.ini >> .render\build.sh
-echo pip install -r requirements.txt >> .render\build.sh
-chmod +x .render\build.sh
+#!/bin/bash
+set -e  # Exit on error
+apt-get update
+apt-get install -y unixodbc unixodbc-dev freetds-bin freetds-dev tdsodbc
+# Verify FreeTDS installation
+ls -l /usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so || echo "libtdsodbc.so not found"
+ls -l /usr/lib/x86_64-linux-gnu/odbc/libtdsS.so || echo "libtdsS.so not found"
+# Configure odbcinst.ini
+echo "[FreeTDS]" > /etc/odbcinst.ini
+echo "Description=FreeTDS ODBC Driver" >> /etc/odbcinst.ini
+echo "Driver=/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so" >> /etc/odbcinst.ini
+echo "Setup=/usr/lib/x86_64-linux-gnu/odbc/libtdsS.so" >> /etc/odbcinst.ini
+echo "TDS_Version=7.4" >> /etc/odbcinst.ini
+# Debug odbcinst.ini
+cat /etc/odbcinst.ini
+pip install -r requirements.txt
