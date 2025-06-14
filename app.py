c8803187ab3abfb2b865cd0d3c9b7b467507a7d5
Arun Nautiyal <nautiyalar@gmail.com>
2025-06-14 12:24:46 +0530

    Update files for ODBC Driver 17 deployment

--- app.py
+++ app.py
@@ -101,6 +101,6 @@ def save_attendance():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
     
-    # ... (existing imports and routes)
+  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
