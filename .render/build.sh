4c6230c997a6b7ac5241f6b8f909dd18ab22834d
Arun Nautiyal <nautiyalar@gmail.com>
2025-06-14 13:34:32 +0530

    Add build.sh for FreeTDS and update files

--- .render/build.sh
+++ .render/build.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
-pip install -r requirements.txt
+echo #!/bin/bash > .render\build.sh
+echo apt-get update >> .render\build.sh
+echo apt-get install -y unixodbc unixodbc-dev freetds-bin freetds-dev tdsodbc >> .render\build.sh
+echo echo \"[FreeTDS]\" \> /etc/odbcinst.ini >> .render\build.sh
+echo echo \"Description=FreeTDS ODBC Driver\" \>> /etc/odbcinst.ini >> .render\build.sh
+echo echo \"Driver=/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so\" \>> /etc/odbcinst.ini >> .render\build.sh
+echo echo \"Setup=/usr/lib/x86_64-linux-gnu/odbc/libtdsS.so\" \>> /etc/odbcinst.ini >> .render\build.sh
+echo echo \"TDS_Version=7.4\" \>> /etc/odbcinst.ini >> .render\build.sh
+echo pip install -r requirements.txt >> .render\build.sh
+chmod +x .render\build.sh
