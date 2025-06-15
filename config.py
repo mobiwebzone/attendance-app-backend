e4e82b8e5a44ec5cfe6f78819996d910428ad832
Arun Nautiyal <nautiyalar@gmail.com>
2025-06-15 10:04:20 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import pyodbc
 
-    # class Config:
-    # SQL_SERVER = '103.25.174.53'  # e.g., 'localhost' or 'server_ip'
-    # DATABASE = 'pragatiweb'
-    # USERNAME = 'sa'
-    # PASSWORD = 'India@123456#'
-    # DRIVER = '{ODBC Driver 17 for SQL Server}'
-    # CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
-
-import os
-
 class Config:
-    SQL_SERVER = os.getenv('SQL_SERVER', '103.25.174.53')
-    DATABASE = os.getenv('DATABASE', 'pragatiweb')
-    USERNAME = os.getenv('DB_USERNAME')
-    PASSWORD = os.getenv('DB_PASSWORD')
+    SQL_SERVER = '103.25.174.53'  # e.g., 'localhost' or 'server_ip'
+    DATABASE = 'pragatiweb'
+    USERNAME = 'sa'
+    PASSWORD = 'India@123456#'
     DRIVER = '{ODBC Driver 17 for SQL Server}'
-    CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};PORT=1433;DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+    CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+
+# import os
+
+# class Config:
+#     SQL_SERVER = os.getenv('SQL_SERVER', '103.25.174.53')
+#     DATABASE = os.getenv('DATABASE', 'pragatiweb')
+#     USERNAME = os.getenv('DB_USERNAME')
+#     PASSWORD = os.getenv('DB_PASSWORD')
+#     DRIVER = '{ODBC Driver 17 for SQL Server}'
+#     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};PORT=1433;DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
