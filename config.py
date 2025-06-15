4a8076518603c795045c7ac6293484eeff82ec3f
Arun Nautiyal <nautiyalar@gmail.com>
2025-06-15 13:36:44 +0530

    Updated for render

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-import pyodbc
-import os
+# import pyodbc
 
 # class Config:
-#     SQL_SERVER = '103.25.174.53'  # e.g., 'localhost' or 'server_ip'
+#     SQL_SERVER = '103.25.174.53'  
 #     DATABASE = 'pragatiweb'
 #     USERNAME = 'sa'
 #     PASSWORD = 'India@123456#'
 #     DRIVER = '{ODBC Driver 17 for SQL Server}'
 #     CONNECTION_STRING = f'DRIVER={DRIVER};SERVER={SQL_SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
-
+import os
 class Config:
-    # Use os.environ.get() to read from environment variables
+    
     SQL_SERVER = os.environ.get('DB_SERVER', 'default_server_if_local_dev')
     DATABASE = os.environ.get('DB_DATABASE', 'default_db_if_local_dev')
     USERNAME = os.environ.get('DB_USERNAME', 'default_user_if_local_dev')
